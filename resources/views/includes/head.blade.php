6937aee70d7e08d5950bf74029a2f8a79a45569a
Prannoy23 <prannoy23@gmail.com>
2015-11-07 04:23:57 -0800

    css,js added

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Scotch">
+
+<title>Super Cool Layouts</title>
+
+<!-- load bootstrap from a cdn -->
+
+<link rel="stylesheet" type="text/css" href="{{asset('css/foundation.css')}}"/>
+ <link rel="stylesheet" type="text/css" href="{{asset('css/normalize.css')}}"/>
+  <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}"/>
+
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,20 @@
+<div class="large-12 columns" id="header-wrapper">
+    <div class="large-4 column">
+        <img src="{{asset('img/manutd-icon.png')}}" />   
+        <span>
+            CloudSync
+        </span>
+    </div>
+    <div id="nav-bar-wrapper" class="large-8 column" >
+        <ul class="button-group right">
+            <li><a href="home.php" class="button">Home</a></li>
+
+            <li><a href="settings.php" class="button">Settings</a></li>
+            <li><a href="#" class="button">FAQ</a></li>
+        </ul>
+
+    </div>
+
+
+</div>  
+
